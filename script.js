4dfc45aa598f8b7cd2c5092e038599ec4d049e74
ahmedalaa166 <147141754+ahmedalaa166@users.noreply.github.com>
2026-04-22 03:16:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* ==== نظام تحديث الموقع وتجاوز الكاش (Auto-Update System) ==== */
-const SITE_VERSION = "2026.04.22.41"; // غير الرقم ده كل ما تعمل تحديث كبير
+const SITE_VERSION = "2026.04.22.52"; // غير الرقم ده كل ما تعمل تحديث كبير
 
 function handleAutoUpdate() {
     const savedVersion = localStorage.getItem('filmak_site_version');
